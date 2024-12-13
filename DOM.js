cf2164437fe2d35e4e4fd6fbf9f25c158cb7fea2
Yanis-Chevallier <yanischevallier2@gmail.com>
2024-12-13 12:01:51 +0100

    D.O.M 2

---
+++ DOM.js
@@ -0,0 +1,23 @@
+const newCatImage = document.createElement('img');
+
+newCatImage.src = "https://placecats.com/408/287";
+
+newCatImage.alt = "chat trop mignon";
+
+document.body.appendChild(newCatImage);
+
+const myDiv = document.querySelector('.myDiv');
+
+myDiv.appendChild(myElement);
+
+newDiv.appendChild(newCatImage);
+
+myElement.remove();
+
+someDiv.style.backgroundColor = "lightblue";
+
+someText.style.fontSize = "20px";
+
+element.classList.add('myClass');
+
+element.classList.remove('myClass');
